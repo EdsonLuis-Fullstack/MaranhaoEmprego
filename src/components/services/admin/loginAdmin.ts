c5d80c14cc325205c4487bec79dcf5a2367422b3
Brayan Dev <onoratogamer@gmail.com>
2025-07-04 16:44:07 -0300

    adm route

--- src/components/services/admin/loginAdmin.ts
+++ src/components/services/admin/loginAdmin.ts
@@ -1,22 +1,19 @@
-"use server"
+"use server";
 
-export async function Login_ADMIN(username:string , senha:string) {
-
-  const response = await fetch(
-    `${process.env.URL_API_SECRET}/addas/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`,
-        "x-api-key": `${process.env.API_KEY_SECRET}`,
-      },
-      body: JSON.stringify({ uuid: uuid_vaga }),
-      cache:"no-cache"
-    }
-  );
+export async function Login_ADMIN(username: string, senha: string) {
+  const response = await fetch(`${process.env.URL_API_SECRET}/addas/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": `${process.env.API_KEY_SECRET_ADMINISTRADOR}`,
+    },
+    body: JSON.stringify({ usuario:username,senha:senha }),
+    cache: "no-cache",
+  });
   const resposta = await response.json();
   console.log(resposta.msg);
 
   return resposta;
 }
+
+
